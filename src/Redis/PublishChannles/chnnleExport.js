d90e12fdf4abe5d875979e854fdb2e5ec26d1d0b
Shivang Tiwari <93948068+Shivang-tiwari-1@users.noreply.github.com>
2025-06-18 16:10:30 +0530

    Update chnnleExport.js
    s

--- src/Redis/PublishChannles/chnnleExport.js
+++ src/Redis/PublishChannles/chnnleExport.js
@@ -1,6 +1,6 @@
 const { ui_update, state_active, message_channel, publish_patient_channel, publish_doctor_channel } = require("./Channle");
 
-exports.pub_sub_channle_Export = () => {
+exports.pub_sub_channle_Export = async() => {
   ui_update();
   state_active();
   message_channel();
