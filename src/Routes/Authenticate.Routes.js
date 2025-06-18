968558445abafe98e8b7579919404dc071bd97cc
Shivang Tiwari <93948068+Shivang-tiwari-1@users.noreply.github.com>
2025-06-18 16:28:16 +0530

    Update Authenticate.Routes.js
    s

--- src/Routes/Authenticate.Routes.js
+++ src/Routes/Authenticate.Routes.js
@@ -1,19 +1,15 @@
-
 const router = require("express").Router();
 const {
   createUser,
   loginUser,
   logout,
   forgotPass,
-  refreshToken,
   refreshAccessToken,
 } = require("../Controllers/Authentication.Controllers");
+
 const { body } = require("express-validator");
 const upload = require("../Middleware/Multer.Middleware");
-const {
-  deoctorAuthentiaction,
-  authentication,
-} = require("../Middleware/auth.Middleware");
+const { authentication } = require("../Middleware/auth.Middleware");
 
 router.post(
   "/createuser",
@@ -25,8 +21,7 @@ router.post(
           throw new Error("Name is too short");
         }
         return true;
-      })
-      .withMessage("Name is too short"),
+      }),
 
     body("email").isEmail().withMessage("Invalid email address"),
 
@@ -34,52 +29,34 @@ router.post(
 
     body("phone")
       .custom((value) => {
-        if (!value && value.trime().length < 10) {
-          throw new Error("invalid phone number");
+        if (!value || value.trim().length < 10) {
+          throw new Error("Invalid phone number");
         }
         return true;
-      })
-      .withMessage("invalid phone number"),
+      }),
+
     body("address")
-      .custom((value) => {
-        if (!value) {
-          throw new Error("address field is required");
-        }
-        return true;
-      })
-      .withMessage("address field is required"),
+      .notEmpty()
+      .withMessage("Address field is required"),
+
     body("role")
-      .custom((value) => {
-        if (!value) {
-          throw new Error("role is required");
-        }
-        return true;
-      })
-      .withMessage("role is required"),
+      .notEmpty()
+      .withMessage("Role is required"),
   ],
   createUser
 );
+
 router.post(
   "/login",
   [
-    body("email")
-      .custom((value) => {
-        if (!value) {
-          throw new Error("email field is empty");
-        }
-        return true;
-      })
-      .withMessage("email field is required"),
-    body("password").custom((value) => {
-      if (!value) {
-        throw new Error("password is required");
-      }
-    }),
+    body("email").notEmpty().withMessage("Email field is required"),
+    body("password").notEmpty().withMessage("Password is required"),
   ],
   loginUser
 );
+
 router.post("/refreshtoken", refreshAccessToken);
 router.post("/logout", authentication, logout);
 router.post("/forgotpass", authentication, forgotPass);
-module.exports = router;
 
+module.exports = router;
